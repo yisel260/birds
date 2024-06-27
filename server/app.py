7f74f7fdd532dfb6621b0e80e2408f7c78d238f5
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-06-27 13:03:56 -0400

    Added get by ID route

--- server/app.py
+++ server/app.py
@@ -22,5 +22,13 @@ class Birds(Resource):
     def get(self):
         birds = [bird.to_dict() for bird in Bird.query.all()]
         return make_response(jsonify(birds), 200)
+    
+class BirdByID(Resource):
+    def get(self, id):
+        bird = Bird.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(bird), 200)
+
+api.add_resource(BirdByID, '/birds/<int:id>')
+
 
 api.add_resource(Birds, '/birds')
